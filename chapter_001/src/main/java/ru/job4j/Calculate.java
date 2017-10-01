fdaab477e49f1983e93227d397bcff3c818b6bf0
Andrew Ermolaev <goodwin039@gmail.com>
2017-10-01 15:23:38 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,12 @@
 package ru.aermolaev;
 
+/**
+* Класс Hello World
+* @author aermolaev
+* @since 01.10.2017
+* @version 1
+*/
+
 public class Calculate{
 	public static void main(String[] args){
 		System.out.println("Hello World!!!");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Andrew Ermolaev (mailto:Ivan00739@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
