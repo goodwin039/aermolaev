300ddad7326ff18365dfeabd9f5812e8c67ec762
Andrew Ermolaev <goodwin039@gmail.com>
2017-10-28 19:42:51 +0300

    3.4. Максимум из трех чисел [#190], использую max только 2 раза.

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -8,7 +8,6 @@ public class Max {
 
     public int max(int first, int second, int third) {
         max(first, second);
-        max(first, third);
-        return max((max(first, second)), (max(first, third)));
+        return max(max(first, second), third);
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+
+import static org.junit.Assert.*;
+
+public class FactorialTest {
+//    @Test
+
+}
