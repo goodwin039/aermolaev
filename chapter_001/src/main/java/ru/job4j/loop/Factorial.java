9689dbd42d747089fa9ba0b671e7b37a5c39b6b7
Andrew Ermolaev <goodwin039@gmail.com>
2017-10-28 20:17:56 +0300

    4.2. Создать программу вычисляющую факториал. [#193]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,5 +1,16 @@
 package ru.job4j.loop;
 
 public class Factorial {
+    public int calc(int n) {
+        int res = 1;
+        if (n == 0) return res;
+
+        if (n > 0) {
+            for (int i = 1; i <= n; i++) {
+                res *= i;
+            }
+        }
+        return res;
+    }
 
 }
--- chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -1,12 +1,21 @@
 package ru.job4j.loop;
 
 import org.junit.Test;
-import org.junit.Test;
 import static org.hamcrest.core.Is.is;
-
 import static org.junit.Assert.*;
 
 public class FactorialTest {
-//    @Test
+    @Test
+    public void ifFactorial0ThanReturn1() {
+        Factorial factorial = new Factorial();
+        int res = factorial.calc(0);
+        assertThat(res, is(1));
+    }
 
+    @Test
+    public void ifFactorialMoreThan0() {
+        Factorial factorial = new Factorial();
+        int res = factorial.calc(5);
+        assertThat(res, is(120));
+    }
 }
